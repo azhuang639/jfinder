b916270ad1c6ece271c37083ee8991e9390c5a08
Alex Zhuang <azhuang639@gmail.com>
2019-11-03 07:37:23 -0500

    changes to filters

--- main.py
+++ main.py
@@ -25,11 +25,15 @@ def search():
     keyword = 'base'
     information = ""
     queryString = ""
+    minAirdate = ""
+    maxAirdate = ""
     if request.method == "POST" and request.form['submit'] == 'Find':
         maxCount = request.form['maxCount']
         keyword = request.form['keyword']
         difficulty = request.form['difficulty']
         category = request.form['category']
+        minAirdate = request.form['minAirdate']
+        maxAirdate = request.form['maxAirdate']
 
         Questions = ["Question"]
         Points = ["Value"]
@@ -38,31 +42,31 @@ def search():
         Airdates = ["Air Date"]
         count = 0
         index = 0
-        #if False:#keyword != "":
-        #    if maxCount == "":
-        #        maxCount = 10
-        #    while count < maxCount:
-        #        information = get("http://jservice.io/api/clues?")
-        #else:
         queryString = "?"
         if difficulty != "":
             queryString = queryString + "&value=" + difficulty
+        if minAirdate != "":
+            queryString = queryString + "&min_date=" + minAirdate
+        if maxAirdate != "":
+            queryString = queryString + "&max_date=" + maxAirdate
         if category != "":
-            categoryID = 0;
-            offsetCount = 0;
+            categoryID = 0
+            offsetCount = 0
             while categoryID == 0:
-                categoryInfo = get("http://jservice.io/api/categories?count=100&offest=" + offsetCount)
+                categoryInfo = get("http://jservice.io/api/categories?count=100&offset=" + str(offsetCount))
                 if categoryInfo == []:
                     categoryID = -1
-                for categoryIndex in range(0,len(categoryInfo)):
-                    if categoryInfo[categoryIndex]["title"].lower() == category.lower():
-                        categoryID = categoryInfo[categoryIndex]["id"]
-                offsetCount+=100
-            queryString = queryString + "&category=" + categoryID
+                else:
+                    for categoryIndex in range(0,len(categoryInfo)):
+                        if str(categoryInfo[categoryIndex]["title"]).lower() == category.lower():
+                            categoryID = categoryInfo[categoryIndex]["id"]
+                    offsetCount+=100
+            queryString = queryString + "&category=" + str(categoryID)
 
 
-        queryString = "http://jservice.io/api/clues" + queryString
-        information = get(queryString)
+        queryString = "http://jservice.io/api/clues" + queryString + "&offset="
+        offsetSearch = 0
+        information = get(queryString+str(offsetSearch))
         if maxCount == "":
             maxCount = 0
         maxCount = min(int(maxCount), len(information))
@@ -74,15 +78,29 @@ def search():
                 #        if index >= len(information):
                 #            return render_template("search.html", searched = keyword, airdates = Airdates, categories = Categories, questions = Questions, points = Points, answers = Answers, title = 'Search', len = len(Questions))
                 #        keySearchText = str(information[index])
-            Categories.append(information[index]["category"]["title"])
-            Questions.append(information[index]["question"])
-            Points.append(information[index]["value"])
-            Answers.append(information[index]["answer"])
-            Airdates.append(information[index]["airdate"][0:10])
+            if keyword != "":
+                keySearchText = str(information[index])
+                if keySearchText.lower().find(keyword.lower()) != -1 and information[index]["question"] != "":
+                    Categories.append(information[index]["category"]["title"])
+                    Questions.append(information[index]["question"])
+                    Points.append(information[index]["value"])
+                    Answers.append(information[index]["answer"])
+                    Airdates.append(information[index]["airdate"][0:10])
+                    count+=1
+            else:
+                Categories.append(information[index]["category"]["title"])
+                Questions.append(information[index]["question"])
+                Points.append(information[index]["value"])
+                Answers.append(information[index]["answer"])
+                Airdates.append(information[index]["airdate"][0:10])
+                count+=1
             index+=1
-            count+=1
             if index >= len(information):
-                return render_template("search.html", searched = keyword, airdates = Airdates, categories = Categories, questions = Questions, points = Points, answers = Answers, title = 'Search', len = len(Questions))
+                index=0
+                offsetSearch+=100
+                information = get(queryString+str(offsetSearch))
+                if information == []:
+                    return render_template("search.html", airdates = Airdates, categories = Categories, questions = Questions, points = Points, answers = Answers, title = 'Search', len = len(Questions))
     return render_template("search.html", airdates = Airdates, categories = Categories, questions = Questions, points = Points, answers = Answers, title = 'Search', len = len(Questions))
 
 @app.route("/favorites")
