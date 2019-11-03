dd5cdfeafef072d690f25b04356fd3805e374857
Alex Zhuang <azhuang639@gmail.com>
2019-11-03 04:16:12 -0500

    more search functionalities, updated home page

--- main.py
+++ main.py
@@ -15,28 +15,31 @@ def get(url):
 def home():
     return render_template('home.html', title='Home')
 
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
-
 @app.route("/search", methods = ["GET", "POST"])
 def search():
-    Questions = ["Questions"]
+    Questions = ["Question"]
     Points = ["Points"]
     Answers = ["Answer"]
+    Categories = ["Category"]
+    Airdates = ["Air Date"]
     keyword = 'base'
     information = ""
-    if request.method == "POST":
+    if request.method == "POST" and request.form['submit'] == 'Find':
         keyword = request.form['keyword']
-        information = get("http://jservice.io/api/random?count=10")
-        Questions = ["Questions"]
+        maxCount = request.form['maxCount']
+        information = get("http://jservice.io/api/random?count=" + maxCount)
+        Questions = ["Question"]
         Points = ["Points"]
         Answers = ["Answer"]
-        for index in range(0, len(information)):
+        Categories = ["Category"]
+        Airdates = ["Air Date"]
+        for index in range(0, int(maxCount)):
+            Categories.append(information[index]["category_id"])
             Questions.append(information[index]["question"])
             Points.append(information[index]["value"])
             Answers.append(information[index]["answer"])
-    return render_template("search.html", searched = keyword, questions = Questions, points = Points, answers = Answers, title = 'Search', len = len(Questions))
+            Airdates.append(information[index]["airdate"][0:10])
+    return render_template("search.html", searched = keyword, airdates = Airdates, categories = Categories, questions = Questions, points = Points, answers = Answers, title = 'Search', len = len(Questions))
 
 @app.route("/favorites")
 def favorites():
